6a92cfe8df80750a40476b2a9349357129e72d53
armelle09 <alelarge@wecareatwork.com>
2022-05-29 20:57:25 +0200

    implement first step

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+// pb avec connect password cluster avant
+mongoose.connect('mongodb+srv://armelle:J7ZLDLzv8RXKbEq9@cluster0.bra5w.mongodb.net/piiquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+module.exports = app;
